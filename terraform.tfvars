04502414eb28e98cf783f74a2c1bd70779a1ac7a
JUN HEO <junheo@JUNui-MacBookPro.local>
2025-06-12 17:59:23 +0900

    진짜 가자

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ami_id      = "ami-05a3b8c9c294ba4c7"
+vpc_id      = "vpc-048a7fb00bbad59d0"  # main-vpc로 태그된 VPC 사용
+subnet_ids  = []  # 서브넷은 VPC 생성 후 자동으로 생성됨
+db_password = "sajupass"
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the resources will be deployed"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for DB subnet group"
+  type        = list(string)
+}
+
+
+variable "db_password" {
+  type = string
+  sensitive = true
+}
